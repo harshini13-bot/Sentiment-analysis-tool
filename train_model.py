71a9083a81eb8064929e4c2ce334df3bc976002f
Your Name <your-email@example.com>
2026-06-09 01:13:57 +0530

    modified files

--- train_model.py
+++ train_model.py
@@ -3,7 +3,7 @@ import pickle
 import re
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
@@ -28,7 +28,7 @@ vectorizer = TfidfVectorizer()
 X_train = vectorizer.fit_transform(X_train)
 X_test = vectorizer.transform(X_test)
 
-model = MultinomialNB()
+model = LogisticRegression(max_iter=1000)
 
 model.fit(X_train, y_train)
 
@@ -44,8 +44,6 @@ y = df["sentiment"]
 vectorizer = TfidfVectorizer()
 
 X_vectorized = vectorizer.fit_transform(X)
-
-model = MultinomialNB()
 
 model.fit(X_vectorized, y)
 
